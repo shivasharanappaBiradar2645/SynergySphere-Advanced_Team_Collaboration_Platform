54dffb5526fd3b5753879574e832075c6d8f8878
pranav <kishrgdesai@gmail.com>
2025-09-06 12:18:20 +0530

    router projects mytask

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Projects from "./pages/Projects.jsx";
+import Task from "./pages/Task.jsx";
+
 
 function App() {
-    return (
-        <>
-            <Projects/>
-        </>
-    )
+  return (
+    <Router>
+     
+      <Routes>
+        <Route path="/" element={<Projects />} />
+        <Route path="/mytask" element={<Task/>} />
+        {/* other routes go here */}
+      </Routes>
+    </Router>
+  );
 }
 
-export default App
+export default App;
