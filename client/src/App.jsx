6610cb4c2198ca62350221d4e757d3cd099da3c5
Swap <108745622+Swapnilnaique@users.noreply.github.com>
2025-09-06 09:09:01 +0530

    client setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import {useState} from 'react'
+import viteLogo from '/vite.svg'
+
+function App() {
+    const [count, setCount] = useState(0)
+
+    return (
+        <>
+            <h1 className={"text-3xl"}>Hello World</h1>
+        </>
+    )
+}
+
+export default App
